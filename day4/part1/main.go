3ab9e0112340a383a14a6ae73b181341d09c5841
= <=>
2023-08-17 11:13:30 -0300

    Finishing part 1 of day 4

---
+++ day4/part1/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readInputAndUseResult(filename string) int {
+	raw_data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Error reading file")
+	}
+	inputData := string(raw_data)
+	raw_data = make([]byte, 0)
+	lines := strings.Split(inputData, "\n")
+	var count int
+	for _, line := range lines {
+		pair := strings.Split(line, ",")
+		first, err := convertStringSliceToIntSlice(strings.Split(pair[0], "-"))
+		if err != nil {
+			log.Fatal("Could not convert string slice to int slice", err)
+		}
+		second, err := convertStringSliceToIntSlice(strings.Split(pair[1], "-"))
+		if err != nil {
+			log.Fatal("Could not convert string slice to int slice", err)
+		}
+		if checkIfOverlaps(first, second) {
+			count++
+		}
+	}
+	return count
+}
+
+func checkIfOverlaps(first []int, second []int) bool {
+	if first[0] >= second[0] && first[1] <= second[1] {
+		return true
+	}
+	if first[0] <= second[0] && first[1] >= second[1] {
+		return true
+	}
+	return false
+}
+func convertStringSliceToIntSlice(first []string) ([]int, error) {
+	var result []int
+	for _, item := range first {
+		conv, err := strconv.Atoi(item)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, conv)
+	}
+	return result, nil
+}
+func main() {
+	fmt.Println(readInputAndUseResult("../input.txt"))
+}
---
+++ day4/part1/main_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "testing"
+
+func TestOverlaps(t *testing.T) {
+	want := false
+	first := []int{2, 4}
+	second := []int{6, 8}
+	got := checkIfOverlaps(first, second)
+	if want != got {
+		t.Errorf("checkIfOverlaps([2,4], [6,8]) = %t, want = %t", got, want)
+	}
+	want = false
+	first = []int{2, 3}
+	second = []int{4, 5}
+	got = checkIfOverlaps(first, second)
+	if want != got {
+		t.Errorf("checkIfOverlaps([2,3], [4,5]) = %t, want = %t", got, want)
+	}
+	want = false
+	first = []int{5, 7}
+	second = []int{7, 9}
+	got = checkIfOverlaps(first, second)
+	if want != got {
+		t.Errorf("checkIfOverlaps([5,7], [7,9]) = %t, want = %t", got, want)
+	}
+	want = true
+	first = []int{2, 8}
+	second = []int{3, 7}
+	got = checkIfOverlaps(first, second)
+	if want != got {
+		t.Errorf("checkIfOverlaps([2,8], [3,7]) = %t, want = %t", got, want)
+	}
+	want = true
+	first = []int{6, 6}
+	second = []int{4, 6}
+	got = checkIfOverlaps(first, second)
+	if want != got {
+		t.Errorf("checkIfOverlaps([6,6], [4,6]) = %t, want = %t", got, want)
+	}
+	want = false
+	first = []int{2, 6}
+	second = []int{4, 8}
+	got = checkIfOverlaps(first, second)
+	if want != got {
+		t.Errorf("checkIfOverlaps([2,6], [4,8]) = %t, want = %t", got, want)
+	}
+
+}
